57bd9e938d827b65b9b4a3673857170552f229a4
hongy <hongy3025@163.com>
2026-06-24 09:46:06 +0800

    refactor(gdapi): migrate builtin_ping to req/res pattern

--- gdapi/addon/runtime/builtin_ping.gd
+++ gdapi/addon/runtime/builtin_ping.gd
@@ -1,9 +1,9 @@
 @tool
 extends "res://addons/gdapi/runtime/route_handler.gd"
 
-func handle(_params: Dictionary) -> Dictionary:
-    return {
+func handle(_req: GdApiRequest, res: GdApiResponse) -> void:
+    res.json({
         "ok": true,
         "gdapi_version": "0.2.0",
         "editor_version": Engine.get_version_info().get("string", ""),
-    }
+    })
