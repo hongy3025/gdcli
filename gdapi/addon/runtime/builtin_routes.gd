946226f361238a12d6581bb1c92117293ae0202b
hongy <hongy3025@163.com>
2026-06-24 09:49:21 +0800

    refactor(gdapi): migrate builtin_routes to req/res pattern

--- gdapi/addon/runtime/builtin_routes.gd
+++ gdapi/addon/runtime/builtin_routes.gd
@@ -4,7 +4,7 @@ extends "res://addons/gdapi/runtime/route_handler.gd"
 var _route_names: Array = []
 
 func set_route_names(names: Array) -> void:
-    _route_names = names
+	_route_names = names
 
-func handle(_params: Dictionary) -> Dictionary:
-    return {"ok": true, "routes": _route_names}
+func handle(_req: GdApiRequest, res: GdApiResponse) -> void:
+	res.json({"ok": true, "routes": _route_names})
